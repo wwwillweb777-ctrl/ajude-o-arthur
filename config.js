c5f2e6ddce85547cc6d47d43ead7dc3041b05d62
wwwillweb777-ctrl <wwwill.web777@gmail.com>
2026-06-09 19:35:05 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js - Apenas configurações gerais
+window.firebaseConfig = {
+    apiKey: "AIzaSyDUgHEX610xqLmEmM6F_d0sOzbYxLLxT8w",
+    authDomain: "willobras.firebaseapp.com",
+    databaseURL: "https://willobras-default-rtdb.firebaseio.com",
+    projectId: "willobras",
+    storageBucket: "willobras.firebasestorage.app",
+    messagingSenderId: "66370500400",
+    appId: "1:66370500400:web:42d6569ca2c8172ddf3af5"
+};
+
+window.SENHA_ADMIN = "WJ321";
