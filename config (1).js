861744383ac6969a2315f5e029dc34be3ac96d1a
wwwillweb777-ctrl <wwwill.web777@gmail.com>
2026-06-10 01:29:54 -0300

    Add files via upload

---
+++ config (1).js
@@ -0,0 +1,15 @@
+// config.js - Apenas configurações externas
+window.firebaseConfig = {
+    apiKey: "AIzaSyDUgHEX610xqLmEmM6F_d0sOzbYxLLxT8w",
+    authDomain: "willobras.firebaseapp.com",
+    databaseURL: "https://willobras-default-rtdb.firebaseio.com",
+    projectId: "willobras",
+    storageBucket: "willobras.firebasestorage.app",
+    messagingSenderId: "66370500400",
+    appId: "1:66370500400:web:42d6569ca2c8172ddf3af5"
+};
+
+window.SENHA_ADMIN = "WJ321";
+
+// Link do Apps Script já existente e funcionando
+window.LINK_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbxhff2kFVLHrE9ieru3Zf0ljarux0nItd30_dY4w_yvDF4gs5Xdy03fa_jxzqAyqBy9/exec";
